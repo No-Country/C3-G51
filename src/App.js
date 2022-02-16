baa9956c28203a60a080e1c8c8f19829d01c63f6
César <carabetta87@gmail.com>
2022-02-16 08:31:48 -0300

    Casi terminado header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from './components/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Header />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
