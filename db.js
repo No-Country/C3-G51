76a6e0142a7544398a1e702804d414c9c46ec388
Jesus Rios <jesus.rios2609@gmail.com>
2022-02-23 18:46:37 -0500

    Update db.js
    Add Environment Variables

--- db.js
+++ db.js
@@ -20,8 +20,8 @@ const
 
 
 
-const sequelize = new Sequelize('L2kV8emHXn', 'L2kV8emHXn', 'oVGZrhch8M',{
-    host: 'remotemysql.com',
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD,{
+    host: process.env.DB_HOST,
     dialect: 'mysql' 
 });
 
